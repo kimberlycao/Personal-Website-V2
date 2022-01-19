af0050c4c605f59755c024739dc91c3471f27204
kimberlycao <kimberly.cao13@gmail.com>
2022-01-19 00:34:23 -0500

    Fixed tailwindcss installation and added some files

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="relative min-h-screen flex">
+      <div className="bg-slate-900 text-blue-100 w-64"></div>
+      <div>Content</div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h1 className="text-5xl font-bold underline">
+      Welcome to My Personal Website!
+    </h1>
+  );
+};
+
+export default Home;
