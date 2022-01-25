7f388ce8ceeb13be50b3f538a19fc7d5a47a4f24
kimberlycao <kimberly.cao13@gmail.com>
2022-01-25 03:50:32 -0500

    Fixed of responsiveness issues

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -1,9 +1,9 @@
 const AnimatedBackground = () => {
   return (
     <div>
-      <div className="absolute top-0 w-96 h-96 bg-pink-300 rounded-full blur-xl opacity-20 animate-blob"></div>
-      <div className="absolute top-0 -right-4 w-96 h-96 bg-pink-400 rounded-full blur-xl opacity-20 animate-blob animation-delay-2000"></div>
-      <div className="absolute -bottom-20 left-28 w-96 h-96 bg-pink-500 rounded-full blur-xl opacity-20 animate-blob animation-delay-4000"></div>
+      <div className="absolute top-0 w-80 h-80 md:w-96 md:h-96 bg-pink-300 rounded-full blur-xl opacity-20 animate-blob"></div>
+      <div className="absolute top-0 -right-4 w-80 h-80 md:w-96 md:h-96 bg-pink-400 rounded-full blur-xl opacity-20 animate-blob animation-delay-2000"></div>
+      <div className="absolute -bottom-20 left-28 w-80 h-80 md:w-96 md:h-96 bg-pink-500 rounded-full blur-xl opacity-20 animate-blob animation-delay-4000"></div>
     </div>
   );
 };
--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -34,13 +34,15 @@ const SocialMediaLinks = () => {
         rel="noreferrer"
       >
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-7 h-7 mb-4 text-pink-400 fill-current hover:-translate-y-1 duration-200 hover:text-pink-300"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+          viewBox="0 0 16 16"
+          version="1.1"
+          data-view-component="true"
+          className="w-6 h-6 mb-4 text-pink-400 fill-current hover:-translate-y-1 duration-200 hover:text-pink-300"
         >
-          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+          <path
+            fill-rule="evenodd"
+            d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+          ></path>
         </svg>
       </a>
       <div className="w-3 h-24 border-r-2 border-solid border-pink-400"></div>
